844468a3e413cb7a3c28380994d4aeb90649e66c
DesmondFur <desfurtick@gmail.com>
2019-09-06 14:31:01 -0600

    imported more skills

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,15 @@ import PropTypes from 'prop-types'
 import { FaNodeJs, FaReact, FaHtml5, FaCss3Alt } from 'react-icons/fa'
 import { GoMarkGithub } from 'react-icons/go'
 import { IoLogoJavascript } from 'react-icons/io'
-
+import git from '../images/git.jpg'
+import express from '../images/express.png'
+import jest from '../images/jest.png'
+import redux from '../images/redux.png'
+import sqltabs from '../images/sql_tabs.png'
+import postman from '../images/postman.png'
 import styled from 'styled-components'
+import ReactPlayer from 'react-player'
+
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <h1>Skills</h1>
@@ -21,22 +28,38 @@ const Footer = props => (
     <SkillsList>Css3</SkillsList>
     <IoLogoJavascript color={'#f7df1e'} size={80} />
     <SkillsList>Javascript</SkillsList>
-    <Image src="https://www.postgresql.org/media/img/about/press/elephant.png" alt=""/>
+    <Image
+      src="https://www.postgresql.org/media/img/about/press/elephant.png"
+      alt="postgresIcon"
+    />
     <SkillsList>PostgreSql</SkillsList>
-    <p className="copyright">
+    <Image src={git} />
+    <SkillsList>Git</SkillsList>
+    <Image src={express} />
+    <SkillsList>Express</SkillsList>
+    <Image src={jest} />
+    <SkillsList>Jest</SkillsList>
+    <Image src={redux} />
+    <SkillsList>Redux</SkillsList>
+    <Image src="https://www.sqltabs.com/img/logo.png" />
+    <SkillsList>SqlTabs</SkillsList>
+    <Image src="https://miro.medium.com/max/512/1*fVBL9mtLJmHIH6YpU7WvHQ.png" />
+    <SkillsList>Postman</SkillsList>
+    {/* <p className="copyright">
       &copy; Gatsby Starter - Dimension. Design:{' '}
       <a href="https://html5up.net">HTML5 UP</a>. Built with:{' '}
       <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
-    </p>
+    </p> */}
   </footer>
 )
 
 const SkillsList = styled.span`
-  font-size: 1em;
+  font-size: .7em;
+  position:relative;
 `
 
 const Image = styled.img`
-width:70px;
+  width: 70px;
 `
 
 Footer.propTypes = {
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { IoLogoLinkedin } from "react-icons/io";
-
+import ReactPlayer from 'react-player'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import psite from '../images/psite.mp4'
 
 class Main extends React.Component {
   render() {
@@ -13,11 +14,14 @@ console.log(this.state)
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
 
     return (
+      
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
+          <h2 className="major">Projects</h2>
+          <ReactPlayer url={psite} playing />
           <span className="image main"><img src={pic02} alt="" /></span>
+          
           <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
           <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
           {close}
@@ -53,7 +57,7 @@ console.log(this.state)
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="https://www.linkedin.com/in/desmond-furtick-b81330168/" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
+            <li><a href="www.linkedin.com/in/DesmonFur" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
             <li><a href="https://github.com/DesmonFur" className="icon fa-github"><span className="label">GitHub</span></a></li>
           </ul>
           {close}
